5e0e72159a68c880abdedff718b76411bf5648fb
connor lee <connorlee184@gmail.com>
2024-12-28 18:40:19 -0800

    edit

--- census_income_demo.py
+++ census_income_demo.py
@@ -78,73 +78,38 @@ class ROCCallback(Callback):
     def on_batch_end(self, batch, logs={}):
         return
 
-
 def data_preparation():
-    # The column names are from
-    # https://www2.1010data.com/documentationcenter/prod/Tutorials/MachineLearningExamples/CensusIncomeDataSet.html
-    column_names = ['age', 'class_worker', 'det_ind_code', 'det_occ_code', 'education', 'wage_per_hour', 'hs_college',
-                    'marital_stat', 'major_ind_code', 'major_occ_code', 'race', 'hisp_origin', 'sex', 'union_member',
-                    'unemp_reason', 'full_or_part_emp', 'capital_gains', 'capital_losses', 'stock_dividends',
-                    'tax_filer_stat', 'region_prev_res', 'state_prev_res', 'det_hh_fam_stat', 'det_hh_summ',
-                    'instance_weight', 'mig_chg_msa', 'mig_chg_reg', 'mig_move_reg', 'mig_same', 'mig_prev_sunbelt',
-                    'num_emp', 'fam_under_18', 'country_father', 'country_mother', 'country_self', 'citizenship',
-                    'own_or_self', 'vet_question', 'vet_benefits', 'weeks_worked', 'year', 'income_50k']
+    label_columns = ['HOSP', 'RDMIT']
 
-    # Load the dataset in Pandas
-    train_df = pd.read_csv(
-        'data/census-income.data.gz',
-        delimiter=',',
-        header=None,
-        index_col=None,
-        names=column_names
-    )
-    other_df = pd.read_csv(
-        'data/census-income.test.gz',
-        delimiter=',',
-        header=None,
-        index_col=None,
-        names=column_names
-    )
+    categorical_columns = ['PROCTYP', 'CAP_SVC', 'FACPROF', 'MHSACOVG', 'NTWKPROV', 'PAIDNTWK', 'ADMTYP', 'MDC', 'DSTATUS',
+                        'PLANTYP', 'MSA', 'AGEGRP', 'EECLASS', 'EESTATU', 'EMPREL', 'PHYFLAG', 'SEX', 'HLTHPLAN', 'INDSTRY',
+                        'OUTPATIENT', 'SEASON', 'STDPROV', 'PROCGRP']
 
-    # First group of tasks according to the paper
-    label_columns = ['income_50k', 'marital_stat']
+    train_raw_labels = pd.read_csv("./data/train_raw_labels.csv")
+    other_raw_labels = pd.read_csv("./data/other_raw_labels.csv") 
+    transformed_train = pd.read_csv("./data/transformed_train.csv") 
+    transformed_other = pd.read_csv("./data/transformed_other.csv") 
 
-    # One-hot encoding categorical columns
-    categorical_columns = ['class_worker', 'det_ind_code', 'det_occ_code', 'education', 'hs_college', 'major_ind_code',
-                           'major_occ_code', 'race', 'hisp_origin', 'sex', 'union_member', 'unemp_reason',
-                           'full_or_part_emp', 'tax_filer_stat', 'region_prev_res', 'state_prev_res', 'det_hh_fam_stat',
-                           'det_hh_summ', 'mig_chg_msa', 'mig_chg_reg', 'mig_move_reg', 'mig_same', 'mig_prev_sunbelt',
-                           'fam_under_18', 'country_father', 'country_mother', 'country_self', 'citizenship',
-                           'vet_question']
-    train_raw_labels = train_df[label_columns]
-    other_raw_labels = other_df[label_columns]
-    transformed_train = pd.get_dummies(train_df.drop(label_columns, axis=1), columns=categorical_columns)
-    transformed_other = pd.get_dummies(other_df.drop(label_columns, axis=1), columns=categorical_columns)
+    train_HOSP = to_categorical((train_raw_labels['HOSP'] == 1).astype(int), num_classes=2)
+    train_RDMIT = to_categorical((train_raw_labels['RDMIT'] == 1).astype(int), num_classes=2)
 
-    # Filling the missing column in the other set
-    transformed_other['det_hh_fam_stat_ Grandchild <18 ever marr not in subfamily'] = 0
-
-    # One-hot encoding categorical labels
-    train_income = to_categorical((train_raw_labels.income_50k == ' 50000+.').astype(int), num_classes=2)
-    train_marital = to_categorical((train_raw_labels.marital_stat == ' Never married').astype(int), num_classes=2)
-    other_income = to_categorical((other_raw_labels.income_50k == ' 50000+.').astype(int), num_classes=2)
-    other_marital = to_categorical((other_raw_labels.marital_stat == ' Never married').astype(int), num_classes=2)
+    other_HOSP = to_categorical((other_raw_labels['HOSP'] == 1).astype(int), num_classes=2)
+    other_RDMIT = to_categorical((other_raw_labels['RDMIT'] == 1).astype(int), num_classes=2)
 
     dict_outputs = {
-        'income': train_income.shape[1],
-        'marital': train_marital.shape[1]
+        'HOSP': train_HOSP.shape[1],
+        'RDMIT': train_RDMIT.shape[1]
     }
     dict_train_labels = {
-        'income': train_income,
-        'marital': train_marital
+        'HOSP': train_HOSP,
+        'RDMIT': train_RDMIT
     }
     dict_other_labels = {
-        'income': other_income,
-        'marital': other_marital
+        'HOSP': other_HOSP,
+        'RDMIT': other_RDMIT
     }
     output_info = [(dict_outputs[key], key) for key in sorted(dict_outputs.keys())]
 
-    # Split the other dataset into 1:1 validation to test according to the paper
     validation_indices = transformed_other.sample(frac=0.5, replace=False, random_state=SEED).index
     test_indices = list(set(transformed_other.index) - set(validation_indices))
     validation_data = transformed_other.iloc[validation_indices]
@@ -154,7 +119,19 @@ def data_preparation():
     train_data = transformed_train
     train_label = [dict_train_labels[key] for key in sorted(dict_train_labels.keys())]
 
+    # # Split the other dataset into 1:1 validation to test according to the paper
+    # validation_indices = transformed_other.sample(frac=0.5, replace=False, random_state=SEED).index
+    # test_indices = list(set(transformed_other.index) - set(validation_indices))
+    # validation_data = transformed_other.iloc[validation_indices]
+    # validation_label = [dict_other_labels[key][validation_indices] for key in sorted(dict_other_labels.keys())]
+    # test_data = transformed_other.iloc[test_indices]
+    # test_label = [dict_other_labels[key][test_indices] for key in sorted(dict_other_labels.keys())]
+    # train_data = transformed_train
+    # train_label = [dict_train_labels[key] for key in sorted(dict_train_labels.keys())]
+
     return train_data, train_label, validation_data, validation_label, test_data, test_label, output_info
+
+
 
 
 def main():
